0de1d2c2260f73c0a58a7f10c65981a6e28724a6
Katherine023 <taty23082002@gmail.com>
2021-10-08 11:32:36 -0600

    Agregar archivos de proyecto.

---
+++ Infraestructure/Interfaces/IProductosModel.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructure.Interfaces
+{
+    public interface IProductosModel:IProductos<Producto>
+    {
+        Producto FindById(int id);
+        int GetLastProductoId();
+
+    }
+}
---
+++ Infraestructure/Productos/PEPS.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructure.Productos
+{
+    public class PEPS
+    {
+        public decimal cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal Importe { get; set; }
+    }
+}
